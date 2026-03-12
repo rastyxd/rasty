530dcc7be53bca37d1114b1441d7b5f12e2ea587
Rasty <rastyxdofficial@gmail.com>
2026-03-12 03:10:42 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#1e1e1e]">
+      <h1 className="text-[#f5f5f5] text-4xl font-bold justify-center flex text-center mt-20">
+        Coming Soon...
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000, // Change the development port
+    strictPort: true, // Exit if the port is already in use
+  },
+  preview: {
+    port: 8080, // Change the port for the production preview server
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    tailwindcss(),
+  ],
+});
